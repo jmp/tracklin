977dc49607bd7e512206bb37d3759f7a88a6d6f9
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2020-04-05 20:19:25 +0300

    Fix timing test

--- src/test/kotlin/com/github/jmp/tracklin/TimingTest.kt
+++ src/test/kotlin/com/github/jmp/tracklin/TimingTest.kt
@@ -2,14 +2,17 @@ package com.github.jmp.tracklin
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.Date
+import java.util.*
 
 class TimingTest {
     @Test
-    fun `getCurrentTimeAsString returns the time as a string`() {
+    fun `getTimeAsString returns the time as a string`() {
+        val calendar = Calendar.getInstance().apply {
+            set(2000, 1, 1, 12, 0, 0)
+        }
         assertEquals(
-            "18:53:57",
-            getTimeAsString(Date(1586102037247L))
+            "12:00:00",
+            getTimeAsString(calendar.time)
         )
     }
 }
